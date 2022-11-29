0770aa9953fbd4297ccb96fe0a168d3a776a0e97
Marah Ashayer <110784795+Marah-Ashayer@users.noreply.github.com>
2022-11-29 21:01:19 +0200

    Add files via upload

---
+++ Fundamentals/Hello World/hello.py
@@ -0,0 +1,18 @@
+#1
+print("Hello World")
+
+#2a
+name="Marah"
+print("Hello",name)
+#2b
+print("Hello"+name)
+#3a
+fav_num=4
+print("Hello",fav_num)
+#3b
+print("Hello"+str(fav_num))
+#4a
+food_one="potato"
+food_two="pizza"
+print("I love to eat {} and {}".format(food_one,food_two))
+print(f"I love to eat {food_one} and {food_two}")
